803892f10233335f8a8f61729bf3e0cfbe3c3941
Lee Reamsnyder <lee.reamsnyder@gmail.com>
2014-09-08 16:06:08 -0400

    Various changes to make elements fit
    + Remove automatic word breaking on pre/code blocks
    + Add utility classes for overflow and word breaking
    + Remove white-space pre-wrap from pre,code,tt elements
    + Remove overflow:hidden from .off-canvas-wrap. I thought this was
    necessary but it doesn't appear to really be. Its presence could cause
    some real damage in trying to read big tables
    + New JS module: "Compactor". It looks for pre/tables that are too
    large and wraps them in an element that allows overflow. It also looks
    for errant code/anchors that are too wide and sets them to break lines.
    This runs on load and window resize
    
    + (unrelated) Headers in .copy are now normal weight instead of bold.
    They stand out enough with the borders
    
    Close #18

--- _harp/assets/js/enhanced.js
+++ _harp/assets/js/enhanced.js
@@ -77,4 +77,5 @@ OSLC.supportsAcceleratedTransitions = Modernizr && "csstransforms3d" in Moderniz
 // @codekit-append "./modules/oslc.deepthroat.js";
 // @codekit-append "./modules/oslc.sextant.js";
 // @codekit-append "./modules/oslc.nag.js";
+// @codekit-append "./modules/oslc.compactor.js";
 // @codekit-append "./modules/oslc.doubtfire.js";
---
+++ _harp/assets/js/modules/oslc.compactor.js
@@ -0,0 +1,43 @@
+;(function ($, window, document, undefined) {
+
+var compactElements = function(){
+
+  var copyBlocks = $('.copy');
+  
+  // First up, tables and pre blocks, which can be wrapped in a wrapper div
+  copyBlocks.find('table, pre').each(function(){
+    var 
+      $el = $(this),
+      copyWidth = $el.closest('.copy').width();
+    
+    if ( $el.width() > copyWidth ) {      
+      $el.wrap('<div class="overflow-wrap"></div>');
+    }
+    
+  });
+  
+  // Second up: some <code> and <a> (especially auto URLs eg <http:somethingreallylong.org>) can get too long and don't necessarily break
+  copyBlocks.find('code, a').each(function(){
+    
+    var 
+      $el = $(this),
+      $parent = $el.parent();
+    
+    // <code> in <pre> has overflow. We're good
+    if ( $parent.is('pre') ) { return; }
+    
+    if ( $el.width() > $parent.width() ) {
+      $el.addClass('line-break-all-the-things');
+    }
+    
+  });
+};
+
+// do it on load
+$(document).ready( compactElements );
+
+// and resize
+// this appears to capture orientation change events as well
+$(window).on( 'resize', _.debounce(compactElements, 500) );
+
+})(jQuery, this, this.document);
